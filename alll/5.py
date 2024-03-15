5a38b7a6a4ca39a8e66c0010ea140f2fb23b53a2
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-03-15 16:52:06 +0700

    one

--- alll/5.py
+++ alll/5.py
@@ -1,17 +1,17 @@
 height=float(input('Введите рост в сантиметрах:'))
 weight=float(input('Введите вес в килограммах:'))
 IMT=(weight/(height**2))
-if IMT<16:
+if int(IMT)<16:
     print('Выраженный дефицит')
-if 16<=IMT<18.49:
+if 16<=int(IMT)<18.49:
     print('Недостаточная')
-if 18.5<=IMT<24.99:
+if 18.5<=int(IMT)<24.99:
     print('Норма')
-if 25<=IMT<29.99:
+if 25<=int(IMT)<29.99:
     print('Избыточная')
-if 30<=IMT<34.99:
+if 30<=int(IMT)<34.99:
     print('Ожирение 1')
-if 35<=IMT<39.99:
+if 35<=int(IMT)<39.99:
     print('Ожирение 2')
-if IMT>=40:
+if int(IMT)>=40:
     print('Ожирение 3')
---
+++ alll/8.py
@@ -0,0 +1,11 @@
+n = int(input("Введите количество кнатов: "))
+
+galleon = n // (17*29)
+sickle = (n % (17 *29)) // 29
+knut = n % 29
+if galleon:
+    print(f"{galleon} галлеонов")
+if sickle:
+    print(f"{sickle} сиклей")
+if knut:
+    print(f"{knut} кнатов")
