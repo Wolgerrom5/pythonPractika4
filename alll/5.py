67d6f1060b679da55cc16101ccbf443f23f68dbe
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-03-15 17:00:30 +0700

    one

--- alll/5.py
+++ alll/5.py
@@ -1,17 +1,17 @@
-height=float(input('Введите рост в сантиметрах:'))
-weight=float(input('Введите вес в килограммах:'))
-IMT=(weight/(height**2))
-if int(IMT)<16:
+height = float(input('Введите рост в сантиметрах:'))
+weight = float(input('Введите вес в килограммах:'))
+IMT = (weight / ((height/100) ** 2))
+if IMT < 16:
     print('Выраженный дефицит')
-if 16<=int(IMT)<18.49:
+elif 16 <= IMT < 18.49:
     print('Недостаточная')
-if 18.5<=int(IMT)<24.99:
+elif 18.5 <= IMT < 24.99:
     print('Норма')
-if 25<=int(IMT)<29.99:
+elif 25 <= IMT < 29.99:
     print('Избыточная')
-if 30<=int(IMT)<34.99:
+elif 30 <= IMT < 34.99:
     print('Ожирение 1')
-if 35<=int(IMT)<39.99:
+elif 35 <= IMT < 39.99:
     print('Ожирение 2')
-if int(IMT)>=40:
+else:
     print('Ожирение 3')
