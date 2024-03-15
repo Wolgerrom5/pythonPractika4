abf32da7e5c14abe0d5b20b44545f0b377b15bda
Wolgerrom5 <sofyaviktorovna26@gmail.com>
2024-03-15 15:04:38 +0700

    one

--- alll/10.py
+++ alll/10.py
@@ -5,8 +5,7 @@ else:
     if len(set(pin)) != 4:
         print('ERROR')
     else:
-        # Проверка на год рождения
-        if int(pin) >= 1900 and int(pin) <= 2050:
+        if 1900 <= int(pin) <= 2050:
             print('ERROR')
         else:
             print('OK')
